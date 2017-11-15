9fcfcb9e379ea7559a52198b7f2fdd1fc85f9fbc
minichouilleur <baptiste.auffray@outlook.com>
2017-11-15 12:14:32 +0100

    Delete requete_8_nb_doc_par_annee_et_langues.sql

--- requete_sql/requete_8_nb_doc_par_annee_et_langues.sql
+++
@@ -1,5 +0,0 @@
-/*Cette requète retourne le nombre de document avec l'équivalent d'un Rollup sur Année et languageCode */
-SELECT YEAR(producedDate) AS Année, languageCode, SUM(nb) AS nbDocument
-FROM MonoFaitsCube
-WHERE halId IS NULL AND docTypeCode IS NULL
-GROUP BY Année DESC, languageCode DESC;
