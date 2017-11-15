28bfed3152bd2c327c447f6faf5f96fe99929210
minichouilleur <baptiste.auffray@outlook.com>
2017-11-15 12:12:36 +0100

    Delete requete_1_GROUP_BY.sql

--- requete_sql/requete_1_GROUP_BY.sql
+++
@@ -1,9 +0,0 @@
-/*Cette requète retourne le nombre de langues différente par domaine et année utilisées dans les documents*/
-
-SELECT YEAR(producedDate) as yearDate, domainCode, domainValue, COUNT(*) as nbLanguage
-FROM(
-	SELECT producedDate, domainCode, languageCode
-	FROM faits
-	group by domainCode, languageCode
-) AS temp NATURAL JOIN domaines
-GROUP BY domainCode;
