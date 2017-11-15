3b43523ff66651c68099571cb727a838e816a1fc
minichouilleur <baptiste.auffray@outlook.com>
2017-11-15 12:15:03 +0100

    Delete vue_mono_noyau.sql

--- requete_sql/tables_vues_sql/vue_mono_noyau.sql
+++
@@ -1,5 +0,0 @@
-/*Requète qui créée une vue sur des éléments de la table de faits pour supprimer les doublons sur le halId*/
-
-CREATE VIEW MonoNoyau AS
-    SELECT DISTINCT(halId), docTypeCode, languageCode, producedDate
-	FROM faits;
