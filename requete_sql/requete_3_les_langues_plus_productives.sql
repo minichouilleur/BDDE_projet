89ff2361b15ee5e854972c2335c238141f5ece9e
minichouilleur <baptiste.auffray@outlook.com>
2017-11-15 12:13:30 +0100

    Delete requete_3_les_langues_plus_productives.sql

--- requete_sql/requete_3_les_langues_plus_productives.sql
+++
@@ -1,6 +0,0 @@
-/*Cette requète retourne le nmbre de documents écrit dans une langue*/
-
-SELECT languageValue, COUNT(*) AS nbDocuments
-FROM MonoNoyau NATURAL JOIN langues
-GROUP BY languageCode
-ORDER BY nbDocuments DESC;
