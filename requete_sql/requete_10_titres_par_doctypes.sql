afe9beed9ffb442dca413cc4d08eb72130a958f2
minichouilleur <baptiste.auffray@outlook.com>
2017-11-15 12:12:19 +0100

    Delete requete_10_titres_par_doctypes.sql

--- requete_sql/requete_10_titres_par_doctypes.sql
+++
@@ -1,8 +0,0 @@
-/*Cette requète permet de classer les documents selon le type du document 
-Le but ici est d'imiter le système qui permet de trier les documents sur le site HAL*/
-
-SELECT docTypeCode, docTypeValue, halId, title
-FROM monofaitscube NATURAL JOIN documents NATURAL JOIN types
-WHERE docTypeCode IS NOT NULL AND docTypeValue IS NOT NULL
-GROUP BY halID
-ORDER BY docTypeCode DESC;
